55d224479a87672045ae1178a6b2dec02195c224
Azsig <yasir_abdulaziz@yahoo.co.id>
2023-08-17 09:54:51 +0700

    finished 08 and 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let punctation = /[\.,?!]/g;
+    let lowSentence = sentence.toLowerCase().replace(punctation, "");
+    let newSentence ='';
+    let flipSentence ='';
+    for (let i = 0; i < lowSentence.length; i++){
+        if (lowSentence[i] == " " ){
+            continue;
+        }
+        newSentence += lowSentence[i];
+    }
+    flipSentence = newSentence.split('').reverse().join('');
+    if (newSentence == flipSentence) return true;
+    else return false;
 
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
