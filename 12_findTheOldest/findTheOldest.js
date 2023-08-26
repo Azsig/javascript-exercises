b248717ea7a8e445333fe5b8781c3265ade1dc58
Azsig <yasir_abdulaziz@yahoo.co.id>
2023-08-26 07:48:48 +0700

    finished 09 to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let Oldest = '';
+    let personAge = 0;
+    for (let i in array){
+        let tempAge = getAge(array[i].yearOfBirth, array[i].yearOfDeath);
+        if(tempAge > personAge) {
+            personAge = tempAge;
+            Oldest = array[i];
+        }
+    }
+    return Oldest;
 };
+
+function getAge(birth, death){
+    let age = 0;
+    if(!death){
+        age = new Date().getFullYear() - birth;
+    }
+    else age = death - birth;
+
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
